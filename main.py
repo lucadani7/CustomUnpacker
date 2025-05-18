2ab9d386b85f6ee41ec8fcf1d1da8ff4e79338b2
lucadani7 <79052990+lucadani7@users.noreply.github.com>
2025-05-18 12:58:54 +0300

    main.py file written.

---
+++ main.py
@@ -0,0 +1,49 @@
+import argparse
+import sys
+
+from custom_unpacker import CustomUnpacker
+
+parser = argparse.ArgumentParser(description="Execute all the functions from CustomUnpacker class")
+subparsers = parser.add_subparsers(dest="command", help="Command which you want to execute")
+
+"""Command for creating an archive."""
+create_parser = subparsers.add_parser("create_archive", help="Create archive with specified files.")
+create_parser.add_argument("-s", "--sources", nargs="+", required=True, help="Files list or directory with archive.")
+create_parser.add_argument("-a", "--archive", required=True, help="Archive name who is about to be created.")
+
+"""Command for listing an archive content."""
+list_parser = subparsers.add_parser("list_content", help="List archive content.")
+list_parser.add_argument("-p", "--path", required=True, help="Archive path.")
+
+"""Command for extracting an archive content."""
+unpack_all_parser = subparsers.add_parser("full_unpack", help="Extract all the files from an archive.")
+unpack_all_parser.add_argument("-p", "--path", required=True, help="Archive path.")
+unpack_all_parser.add_argument("-d", "--destination", required=True, help="Destination directory.")
+
+"""Command for extracting some specified files from an archive."""
+unpack_parser = subparsers.add_parser("unpack", help="Extract some specified files from an archive.")
+unpack_parser.add_argument("-p", "--path", required=True, help="Archive path.")
+unpack_parser.add_argument("-f", "--files", nargs="+", required=True, help="Files list who is / are about to be extracted.")
+unpack_parser.add_argument("-d", "--destination", required=True, help="Destination directory.")
+
+unpacker = CustomUnpacker()
+
+while True:
+    cmd_input = input("\nType a command (type 'quit' to quit): ").strip()
+    if cmd_input.lower() == "quit":
+        print("You chose to quit the program. Goodbye!")
+        sys.exit(0)
+
+    args = parser.parse_args(cmd_input.split())
+    match args.command:
+        case "create_archive":
+            unpacker.create_archive(args.sources, args.archive)
+        case "list_content":
+            unpacker.list_content(args.path)
+        case "full_unpack":
+            unpacker.full_unpack(args.path, args.destination)
+        case "unpack":
+            unpacker.unpack(args.path, args.files, args.destination)
+        case _:
+            print(f"Error: Command '{args.command}' is not recognized.")
+            parser.print_help()
